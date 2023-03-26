8f592afa79e57bb5121cb4325cb0f54ba85fad04
Jonathan Jin <jonathanjin55@gmail.com>
2023-03-26 01:18:19 -0400

    added panel for the button

--- trafficmgmt/src/main/java/trafficmgmt/SwingUI.java
+++ trafficmgmt/src/main/java/trafficmgmt/SwingUI.java
@@ -3,12 +3,17 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class SwingUI extends JFrame implements ActionListener{
     static JButton button;  // Global variable for the button
+    static JLabel label;    // Global variable for the label
+    static JPanel panel;    // Global variable for the panel
     public static void main(String[] args) {
 
         // below is the main frame of the UI
@@ -26,15 +31,28 @@ public class SwingUI extends JFrame implements ActionListener{
 
         // below is the button for the admin login
         button = new JButton();
-        button.setBounds(100, 100, 200, 40);
+        button.setBounds(100, 100, 100, 100);
         button.addActionListener(new SwingUI());
         button.setFocusable(false);
         button.setText("Admin Login");
+        button.setHorizontalAlignment(JButton.CENTER);
+        button.setVerticalAlignment(JButton.CENTER);
+        button.setBorder(BorderFactory.createEtchedBorder());
         frame.add(button);
+        
+        // below is the label for the admin login
+        label = new JLabel();
+
+        // below is the panel for the admin login
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 100, 300));
+        panel.setLayout(null);
+        panel.add(button);
+        frame.add(panel);
         }
 
         public void actionPerformed(ActionEvent e) {
-            // place holder method for the admin login
+            // place holder method for the admin login button
             if (e.getSource() == button) {
                 System.out.println("Admin Login");
             }
