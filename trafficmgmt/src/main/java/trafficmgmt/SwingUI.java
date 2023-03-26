15d6987fd276620e01430d3e15f8439ee0adf473
Jonathan Jin <jonathanjin55@gmail.com>
2023-03-25 21:32:40 -0400

    New changes to UI and added a place holder icon

--- trafficmgmt/src/main/java/trafficmgmt/SwingUI.java
+++ trafficmgmt/src/main/java/trafficmgmt/SwingUI.java
@@ -1,9 +1,21 @@
 package trafficmgmt;
+import java.awt.Color;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class SwingUI {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setVisible(true);
-    }
+        frame.setTitle("Traffic Management System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+
+        // below are some visual aspects(Icon,background color) of the UI tbd
+        // ImageIcon icon = new ImageIcon("place_holder_icon.png");
+        // frame.setIconImage(icon.getImage());
+        // frame.getContentPane().setBackground(new Color(0,0,0));
+
+        
+        }
 }
