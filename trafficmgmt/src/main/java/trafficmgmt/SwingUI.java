17c72ec361cabff072482207b31e2d9fe12aa382
Jonathan Jin <jonathanjin55@gmail.com>
2023-03-26 14:04:48 -0400

    Completed the login page for admin

--- trafficmgmt/src/main/java/trafficmgmt/SwingUI.java
+++ trafficmgmt/src/main/java/trafficmgmt/SwingUI.java
@@ -9,19 +9,23 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class SwingUI extends JFrame implements ActionListener{
-    static JButton button;  // Global variable for the button
-    static JLabel label;    // Global variable for the label
-    static JPanel panel;    // Global variable for the panel
+    static JButton loginButton;  // Global variable for the loginButton
+    static JLabel idLabel;    // Global variable for the label
+
     public static void main(String[] args) {
 
-        // below is the main frame of the UI
-        JFrame frame = new JFrame();
-        frame.setVisible(true);
-        frame.setTitle("Traffic Management System");
+        // below is the main frame and panel of the UI
+        JFrame frame = new JFrame("Traffic Management System");
+        JPanel panel = new JPanel();
+        // frame.setTitle("Traffic Management System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,800);
+        frame.add(panel);
+
+        placeComponents(panel);
 
         // below are some visual aspects(Icon,background color) of the UI tbd
 
@@ -29,31 +33,37 @@ public class SwingUI extends JFrame implements ActionListener{
         // frame.setIconImage(icon.getImage());
         // frame.getContentPane().setBackground(new Color(0,0,0));
 
-        // below is the button for the admin login
-        button = new JButton();
-        button.setBounds(100, 100, 100, 100);
-        button.addActionListener(new SwingUI());
-        button.setFocusable(false);
-        button.setText("Admin Login");
-        button.setHorizontalAlignment(JButton.CENTER);
-        button.setVerticalAlignment(JButton.CENTER);
-        button.setBorder(BorderFactory.createEtchedBorder());
-        frame.add(button);
-        
-        // below is the label for the admin login
-        label = new JLabel();
 
-        // below is the panel for the admin login
-        panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 100, 300));
-        panel.setLayout(null);
-        panel.add(button);
-        frame.add(panel);
+        frame.setVisible(true);
+        }
+
+        private static void placeComponents(JPanel panel) {
+            panel.setLayout(null);
+
+            JLabel IDLabel = new JLabel("ID");
+            IDLabel.setBounds(270, 300, 80, 25);
+            panel.add(IDLabel);
+
+            JTextField IDText = new JTextField(20);
+            IDText.setBounds(300, 300, 165, 25);
+            panel.add(IDText);
+
+            JLabel passwordLabel = new JLabel("Password");
+            passwordLabel.setBounds(230, 350, 80, 25);
+            panel.add(passwordLabel);
+
+            JTextField passwordText = new JTextField(20);
+            passwordText.setBounds(300, 350, 165, 25);
+            panel.add(passwordText);
+    
+            loginButton = new JButton("Admin Login");
+            loginButton.setBounds(300, 400, 160, 40);
+            panel.add(loginButton);
         }
 
         public void actionPerformed(ActionEvent e) {
-            // place holder method for the admin login button
-            if (e.getSource() == button) {
+            // place holder method for the admin login loginButton
+            if (e.getSource() == loginButton) {
                 System.out.println("Admin Login");
             }
         }
