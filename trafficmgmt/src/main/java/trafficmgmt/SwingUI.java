f23e1faafd75e197ed7d88bafdc14a0bf90e32b4
Jonathan Jin <jonathanjin55@gmail.com>
2023-03-26 18:14:49 -0400

    Apon successful login, UI switch from login panel to admin panel

--- trafficmgmt/src/main/java/trafficmgmt/SwingUI.java
+++ trafficmgmt/src/main/java/trafficmgmt/SwingUI.java
@@ -1,5 +1,8 @@
 package trafficmgmt;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,31 +15,37 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class SwingUI extends JFrame{
-    static JButton loginButton;  // Global variable for the loginButton
-    static JLabel idLabel;    // Global variable for the label
 
     public static void main(String[] args) {
 
-        // below is the main frame and panel of the UI
+        // below is the main frame and panels of the UI
         JFrame frame = new JFrame("Traffic Management System");
-        JPanel panel = new JPanel();
-        // frame.setTitle("Traffic Management System");
+        JPanel loginPanel = new JPanel();
+        JPanel adminPanel = new JPanel();
+        adminPanel.setBackground(Color.red);
+        JPanel panelContainer = new JPanel();
+        CardLayout cl = new CardLayout();
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,800);
-        frame.add(panel);
+        panelContainer.setLayout(cl);
+        panelContainer.add(loginPanel, "login");
+        panelContainer.add(adminPanel, "admin");
+        cl.show(panelContainer, "login");
 
-        placeComponents(panel);
+
+        loginPanelComponents(loginPanel, cl, panelContainer);
 
         // below are some visual aspects(Icon,background color) of the UI tbd
 
         // ImageIcon icon = new ImageIcon("place_holder_icon.png");
         // frame.setIconImage(icon.getImage());
         // frame.getContentPane().setBackground(new Color(0,0,0));
-
+        frame.add(panelContainer);
         frame.setVisible(true);
         }
 
-        private static void placeComponents(JPanel panel) {
+        private static void loginPanelComponents(JPanel panel, CardLayout cl, JPanel panelContainer) {
             panel.setLayout(null);
 
             JLabel IDLabel = new JLabel("ID");
@@ -56,10 +65,10 @@ public class SwingUI extends JFrame{
             panel.add(passwordText);
 
             JLabel loginConfirmation = new JLabel("");
-            loginConfirmation.setBounds(300, 500, 300, 25);
+            loginConfirmation.setBounds(300, 450, 300, 25);
             panel.add(loginConfirmation);
 
-            loginButton = new JButton("Admin Login");
+            JButton loginButton = new JButton("Admin Login");
             loginButton.setBounds(300, 400, 160, 40);
             panel.add(loginButton);
 
@@ -67,12 +76,12 @@ public class SwingUI extends JFrame{
                 public void actionPerformed(ActionEvent e) {
                     String ID = IDText.getText();
                     String password = passwordText.getText();
+                    Boolean admin2 = ID.equals("admin.2") && password.equals("123456");
 
-                    if(ID.equals("admin.1") && password.equals("123456")) {
-                        loginConfirmation.setText("Login Successful");
-                    } else if(ID.equals("admin.1") && password.equals("123456")) {
-                        loginConfirmation.setText("Login Successful");
-                    } 
+                    if(ID.equals("admin.1") && password.equals("123456") || admin2) {
+                        cl.show(panelContainer, "admin");
+                        
+                    }
                     else {
                         loginConfirmation.setText("Password or ID is incorrect");
                     }
@@ -81,3 +90,4 @@ public class SwingUI extends JFrame{
         }
 
 }
+
